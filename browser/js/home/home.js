b7834a78dc24293befae7fdcbea427de87388b66
Chris Heller <ctheller12@gmail.com>
2016-08-05 11:58:32 -0400

    Finally getting graph to display nicely, with numbers in histogram, and resize effective

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -46,9 +46,9 @@ app.controller('HomeCtrl', function($window, $rootScope, $scope, SpotifyRetrieve
 			$scope.render($scope.tracks);
 		});
 
-    	var margin = 20;
-		var barWidth = 3;
-		var barPadding = 0;
+    	var margin = 30;
+		var barWidth = 20;
+		var barPadding = 2;
 
 		// Use the category20() scale function for multicolor support
        	var color = d3.scaleOrdinal(d3.schemeCategory20);
@@ -61,40 +61,77 @@ app.controller('HomeCtrl', function($window, $rootScope, $scope, SpotifyRetrieve
 		   	if (!data) return;
 		 
 		    // setup variables
-		var height = d3.select('#chart').node().offsetHeight - margin;
-		        // calculate the height
-       	var width = $scope.tracks.length * (barWidth + barPadding);
+		 //var height = d3.select('#chart').node().offsetHeight - margin;
+		//         // calculate the height
+  //      	var width = $scope.tracks.length * (barWidth + barPadding);
         
-        // our xScale
-       	var yScale = d3.scaleLinear()
-         	.range([600, 0]);
+  //       
+
+  			var height = window.innerHeight-100;
+  			var width = window.innerWidth;
+
+  			barWidth = width/60;
+
+  			var xScale = d3.scaleLinear()
+  				.domain([0,1])
+			    .range([0, width]);
+
+			var histogram = d3.histogram()
+								.domain(xScale.domain())
+								.thresholds(xScale.ticks(40));
+
+			var data = histogram(data.map(function(d){return d.danceability}));
+
+			console.log(data);
 		 
-		    // set the height based on the calculations above
+		//     // set the height based on the calculations above
 		   	svg.attr('height', width);
+
+
+			var yScale = d3.scaleLinear()
+			    .domain([0, d3.max(data, function(d) { return d.length; })])
+			    .range([height, 10]);
+
+			//var formatCount = d3.format(",.0f")
+
+			var bar = svg.selectAll(".bar")
+			    .data(data)
+			 	.enter().append("g")
+			    .attr("class", "bar")
+			    //.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 		 
-		    //create the rectangles for the bar chart
-		   	svg.selectAll('rect')
-		     	.data(data).enter()
-		       	.append('rect')
+		//     //create the rectangles for the bar chart
+		   	// svg.selectAll('rect')
+		    //  	.data(data).enter()
+
+		      bar.append('rect')
 		       	.attr('width', barWidth)
 		       	.attr('height', 0)
-		       	.attr('y', 600)
-		       	.attr('x', function(d,i) {
-		         	return i * (barWidth + barPadding);
-		       	})
-		       	.attr('fill', function(d) { return color(d.danceability); })
+		       	.attr('x', function(d, i){ return (barWidth+barPadding)*i+margin})
+		       	.attr('y', height)
+		       	.attr('fill', function(d) { return color(d); })
 		       	.transition()
 		         	.duration(800)
-		         	.attr('y', function(d) {
-		           	return 600 - yScale(d.danceability);
-		        	})
-		        	.attr('height', function(d) {
-		           	return yScale(d.danceability);
-		        	});
+		         	.attr('y', function(d){return yScale(d.length)})
+		        	.attr('height', function(d){return height - yScale(d.length)})
 
-		    svg.selectAll('rect')
-		    .on("mouseover", handleMouseOver)
-            .on("mouseout", handleMouseOut);
+		      bar.append("text")
+			    .attr("dy", ".75em")
+			    .attr("y", height)
+			    .attr("x", function(d, i){ return (barWidth+barPadding)*i+margin+barWidth/2})
+			    .attr("text-anchor", "middle")
+			    .text(function(d) { if (d.length) return d.length })
+		      	.transition()
+		        .duration(800)
+			    .attr("y", function(d){return yScale(d.length)+5})	
+
+		    // svg.selectAll('rect')
+		    // .on("mouseover", handleMouseOver)
+      //       .on("mouseout", handleMouseOut);
+
+  
+
+
 
 
 
@@ -121,7 +158,7 @@ app.controller('HomeCtrl', function($window, $rootScope, $scope, SpotifyRetrieve
 
 	function handleMouseOut(d, i) {
 	    // Use D3 to select element, change color back to normal
-	    d3.select(this).attr('fill', color(d.tempo));
+	    d3.select(this).attr('fill', color(d.danceability));
 
 	    // // Select text by id and then remove
 	  //   // d3.select("#t" + d.id).remove();  // Remove text location
